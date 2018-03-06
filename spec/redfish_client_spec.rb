1dbe527608e4341f2950d58334e024dae839fb97
Tadej Borovšak <tadej.borovsak@xlab.si>
2018-03-06 13:59:44 +0100

    Remove useless, auto-generated test
    Test in question has been generated by bundler and serves no real
    purpose, so we got rid of it.

--- spec/redfish_client_spec.rb
+++ spec/redfish_client_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe RedfishClient do
   it "has a version number" do
     expect(RedfishClient::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
