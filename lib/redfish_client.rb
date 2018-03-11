9ab0be875c4a8c05bfd1de90bf37a038e19c1550
Tadej Borovšak <tadej.borovsak@xlab.si>
2018-03-11 23:02:46 +0100

    Add factory method for creating root resource

--- lib/redfish_client.rb
+++ lib/redfish_client.rb
@@ -1,5 +1,10 @@
+require "redfish_client/connector"
+require "redfish_client/root"
 require "redfish_client/version"
 
 module RedfishClient
-  # Your code goes here...
+  def self.new(url, prefix = "/redfish/v1")
+    con = Connector.new(url)
+    Root.new(con, oid: prefix)
+  end
 end
