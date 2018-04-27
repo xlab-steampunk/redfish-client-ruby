59d04d243e88a5f3c9aebaf066b38a85e1fbb31f
Tadej Borovšak <tadej.borovsak@xlab.si>
2018-04-27 14:50:21 +0200

    Implement basic HTTP authentication support
    Up until now, Redfish client only supported session authentication,
    since it is mandatory as per Redfish standard [1]. This means that
    client can interact with any Redfish-compliant service.
    
    Unfortunately, services in the wild often only implement part of the
    standard and in case of authentication, some of the services only
    offer basic authentication, which makes our client useless for such
    cases.
    
    To bypass this, we added basic authentication support. By default, we
    try to use session authentication if possible, since it is more secure
    of the two, and only use basic authentication if session cannot be
    created.
    
    [1] http://redfish.dmtf.org/schemas/DSP0266_1.4.0.html#authentication

--- spec/redfish_client/root_spec.rb
+++ spec/redfish_client/root_spec.rb
@@ -37,6 +37,18 @@ RSpec.describe RedfishClient::Root do
       { status: 200, body: { "key" => "val" }.to_json }
     )
     Excon.stub(
+      { path: "/basic_root" },
+      { status: 200, body: { "res" => { "@odata.id" => "/basic" } }.to_json }
+    )
+    Excon.stub(
+      { path: "/basic" },
+      { status: 401, body: { "error" => "no auth" }.to_json }
+    )
+    Excon.stub(
+      { path: "/basic", headers: { "Authorization" => "Basic dXNlcjpwYXNz" } },
+      { status: 200, body: { "key" => "basic_val" }.to_json }
+    )
+    Excon.stub(
       { path: "/find" },
       { status: 200, body: { "find" => "resource" }.to_json }
     )
@@ -46,23 +58,46 @@ RSpec.describe RedfishClient::Root do
     Excon.stubs.clear
   end
 
+  context "with sessions" do
+    let(:connector) { RedfishClient::Connector.new("http://example.com") }
+    subject { described_class.new(connector, oid: "/") }
+    before { subject.login("user", "pass") }
+
+    context "#login" do
+      it "authenticates user against service" do
+        expect(subject.Auth.key).to eq("val")
+      end
+    end
+
+    context "#logout" do
+      it "terminates user session" do
+        subject.logout
+        expect { subject.Auth }.to raise_error(Excon::Error::StubNotFound)
+      end
+    end
+  end
+
+  context "without sessions" do
+    let(:connector) { RedfishClient::Connector.new("http://example.com") }
+    subject { described_class.new(connector, oid: "/basic_root") }
+    before { subject.login("user", "pass") }
+
+    context "#login" do
+      it "authenticates user against service" do
+        expect(subject.res.key).to eq("basic_val")
+      end
+    end
+
+    context "#logout" do
+      it "terminates user session" do
+        subject.logout
+        expect(subject.res.error).to eq("no auth")
+      end
+    end
+  end
+
   let(:connector) { RedfishClient::Connector.new("http://example.com") }
   subject { described_class.new(connector, oid: "/") }
-
-  context "#login" do
-    it "authenticates user against service" do
-      subject.login("user", "pass")
-      expect(subject.Auth.key).to eq("val")
-    end
-  end
-
-  context "#logout" do
-    it "terminates user session" do
-      subject.login("user", "pass")
-      subject.logout
-      expect { subject.Auth }.to raise_error(Excon::Error::StubNotFound)
-    end
-  end
 
   context "#find" do
     it "fetches resource by OData id" do
